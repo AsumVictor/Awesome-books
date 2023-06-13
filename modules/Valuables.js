88ee214d3ffe8bafc2e5dbd16dab971b84e8ff84
AsumVictor <iamasum369@gmail.com>
2023-06-13 11:23:14 +0000

    Seperate code into modules

---
+++ modules/Valuables.js
@@ -0,0 +1,7 @@
+export const titleInput = document.querySelector('.title');
+export const authorInput = document.querySelector('.author');
+export const form = document.querySelector('form');
+export const booksListElement = document.querySelector('.listOfBooks');
+export const navigationLinks = document.querySelectorAll('.nav-link');
+export const timeIndicator = document.querySelector('.timeIndicator');
+
